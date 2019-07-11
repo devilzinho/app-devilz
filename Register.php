88c103f6881440ba29c42a46fa99613f89d1d462
devilzinho <52800285+devilzinho@users.noreply.github.com>
2019-07-11 22:08:50 +0200

    Register.php

---
+++ Register.php
@@ -0,0 +1,8 @@
+<?php
+
+//////////////////////////////////// non toccare niente ////////////////////////////////////////
+$token = "774419724:AAGuwyci9C4xjUQVETZTqfkyLEO32tESJfc"; 
+$sito = "https://app-devilz.herokuapp.com/index.php";
+$webhook = 'https://api.telegram.org/bot'.$token.'/setwebhook?url='.$sito.'?api=bot'.$token;
+$ch = curl_init("$webhook");
+curl_exec($ch);
